b65757a7f2ea94d4c9080b786775b565aa73c752
HariShankarSrungavarapu <s.harishankar333@gmail.com>
2017-01-19 16:29:56 +0530

    Added phonelib

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'activeadmin', github: 'activeadmin'
 gem 'inherited_resources', github: 'activeadmin/inherited_resources'
+gem 'phonelib'
 
 group :development, :test do
   gem 'byebug', platform: :mri
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :user
+  validates :number_of_cans, :water_brand, :address, :mobile, :presence => true
+  validates :mobile, phone: { possible: true, types: [:voip, :mobile], country_specifier: -> phone { "IN" } }
 end
