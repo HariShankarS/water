e6ed5b812c97c9329fe513c1354e0ddf6c99bd2e
HariShankarSrungavarapu <s.harishankar333@gmail.com>
2017-01-27 23:22:47 +0530

    rvm set

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,6 +8,7 @@ set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 set :format, :pretty
 set :log_level, :info
+set :rvm_ruby_version, '2.2.2'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
